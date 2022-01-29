6db09790d881f26d61405e65195892f83be0bb41
titicarabina <87486181+titicarabina@users.noreply.github.com>
2022-01-29 14:20:16 -0600

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -12,17 +12,34 @@ QBCore.Functions.CreateCallback('qb-weldingbar:server:GetCheckpointsConfig', fun
     cb(Config.Checkpoints)
 end)
 
+RegisterNetEvent('qb-weldingbar:server:StartWelding', function(point, bool)
+    Config.Checkpoints[point]["opened"] = bool
+    TriggerClientEvent('qb-weldingbar:client:SetBusyState', -1, point, bool)
+end)
+
+QBCore.Functions.CreateCallback('qb-weldingbar:server:HasItems', function(source, cb)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local weedak47 = Player.Functions.GetItemByName("tubautogen")
+    local rollingpaper = Player.Functions.GetItemByName("aparatsudura")
+    if weedak47 ~= nil and rollingpaper ~= nil then
+        cb(true)
+    else
+        cb(false)
+    end
+end)
+
 RegisterNetEvent('qb-weldingbar:server:SetBusyState', function(point, bool)
     Config.Checkpoints[point]["opened"] = bool
     TriggerClientEvent('qb-weldingbar:client:SetBusyState', -1, point, bool)
 end)
 
-RegisterNetEvent('qb-weldingbar:server:RemoveItem', function(source)
+RegisterNetEvent('qb-weldingbar:server:RemoveItem', function(point)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local itemInfo2 = QBCore.Shared.Items["tubautogen"]
-    Player.Functions.RemoveItem("tubautogen", 1)
-    TriggerClientEvent('inventory:client:ItemBox', src, itemInfo, "remove")
+    Player.Functions.RemoveItem('tubautogen', 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, itemInfo2, "remove")
 end)
 
 RegisterNetEvent('qb-weldingbar:server:searchCheckpoint', function(point)
